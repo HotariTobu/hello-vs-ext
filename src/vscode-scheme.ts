cb1facb9d616501849210cc965f35670478867cf
HotariTobu <hotari24tools@gmail.com>
2023-08-15 14:32:10 +0900

    fix: vscode scheme

--- src/vscode-scheme.ts
+++ src/vscode-scheme.ts
@@ -3,12 +3,14 @@ import * as vscode from 'vscode';
 export default (context: vscode.ExtensionContext) => {
     context.subscriptions.push(vscode.window.registerUriHandler({
         handleUri(uri) {
-            vscode.commands.executeCommand(uri.path, ...uri.query.split('&'))
+            vscode.window.showInformationMessage(uri.toString())
+            vscode.window.showInformationMessage(JSON.stringify(uri.toJSON(), null, '    '))
+            vscode.commands.executeCommand(uri.path.replace('/', ''), ...uri.query.split('&'))
         },
     }))
 
     context.subscriptions.push(vscode.commands.registerCommand('hello-vs-ext.redirect', async () => {
-        const uri = 'vscode://hotari.hello-vs-ext/echo?message=hello'
+        const uri = 'vscode://hotari.hello-vs-ext/hello-vs-ext.echo?message=hello'
         vscode.env.openExternal(vscode.Uri.parse(uri))
     }))
 }
